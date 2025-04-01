54b69fab9d408d9fac3c2846c9f29b72cc35ff32
hgolfetto <golfettohenrique@gmail.com>
2025-03-31 21:09:42 -0300

    fix: finish quiz

--- quiz/quiz.java
+++ quiz/quiz.java
@@ -9,13 +9,21 @@ public class quiz {
         int pontosJogador1 = 0, pontosJogador2 = 0;
   
    JOptionPane.showMessageDialog(null, "Bem-vindo ao Jogo de Quiz!", "Quiz", JOptionPane.INFORMATION_MESSAGE);
-
+   
    jogador1 = JOptionPane.showInputDialog(null,"Qual é o seu nome, Jogador 1?",  "Usuário", JOptionPane.QUESTION_MESSAGE);
    jogador2 = JOptionPane.showInputDialog(null,"Qual é o seu nome, Jogador 2?",  "Usuário", JOptionPane.QUESTION_MESSAGE);
    
    String[][] perguntas = {
        {"Qual palavra-chave é usada para definir uma classe em Java?", "class"},
-      
+       {"Qual tipo de dado é usado para armazenar números inteiros em Java?", "int"},
+       {"Qual é o nome do método principal de um programa Java?", "main"},
+       {"Qual é o valor padrão de uma variável do tipo boolean em Java?", "false"},
+       {"Qual operador é usado para comparar se dois valores são iguais em Java?", "=="},
+       {"Qual palavra-chave é usada para criar um laço de repetição em Java?", "for"},
+       {"Qual estrutura de repetição é usada para executar um bloco de código enquanto uma condição for verdadeira?", "while"},
+       {"Qual palavra-chave é usada para sair de um loop antes do término normal?", "break"},
+       {"Qual tipo de dado é usado para armazenar caracteres em Java?", "char"},
+       {"Qual símbolo é usado para comentar uma linha em Java?", "//"},
    };
    
    for (int i = 0; i < perguntas.length; i++) {
